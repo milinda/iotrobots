1300c45dcde228e373e4cdb1b0e2a5498c262717
supunkamburugamuva <supun06@gmail.com>
2014-06-19 13:09:34 -0400

    adding turtlebot files

---
+++ turtlebot/processor/src/main/java/cgl/iotrobots/turtlebot/storm/ObjectAvoidanceTopology.java
@@ -0,0 +1,200 @@
+package cgl.iotrobots.turtlebot.storm;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import com.ss.rabbitmq.*;
+import org.apache.commons.codec.binary.Base64;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ObjectAvoidanceTopology {
+    public static class PrintingBolt extends BaseRichBolt {
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        }
+
+        @Override
+        public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+
+        }
+
+        @Override
+        public void execute(Tuple tuple) {
+
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        TopologyBuilder builder = new TopologyBuilder();
+        ErrorReporter r = new ErrorReporter() {
+            @Override
+            public void reportError(Throwable t) {
+                t.printStackTrace();
+            }
+        };
+
+        builder.setSpout("recv_spout", new RabbitMQSpout(new SpoutConfigurator(), r), 1);
+        builder.setBolt("image_proc", new ObjectDetectionBolt()).shuffleGrouping("recv_spout");
+        builder.setBolt("send_bolt", new PrintingBolt()).shuffleGrouping("image_proc");
+
+        Config conf = new Config();
+        conf.setDebug(false);
+
+
+        if (args != null && args.length > 0) {
+            conf.setNumWorkers(3);
+            StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+        } else {
+            conf.setMaxTaskParallelism(3);
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("image_proc", conf, builder.createTopology());
+            Thread.sleep(1000000);
+            cluster.shutdown();
+        }
+    }
+
+    private static class Base64Builder implements MessageBuilder {
+        @Override
+        public List<Object> deSerialize(RabbitMQMessage message) {
+            byte []body = message.getBody();
+            List<Object> tuples = new ArrayList<Object>();
+
+            byte[] encodedBytes = Base64.encodeBase64(body);
+
+            tuples.add(new String(encodedBytes));
+            return tuples;
+        }
+
+        @Override
+        public RabbitMQMessage serialize(Tuple tuple) {
+            return null;
+        }
+    }
+
+    private static class SpoutConfigurator implements RabbitMQConfigurator {
+        private String url = "amqp://localhost:5672";
+
+        @Override
+        public String getURL() {
+            return url;
+        }
+
+        @Override
+        public boolean isAutoAcking() {
+            return true;
+        }
+
+        @Override
+        public int getPrefetchCount() {
+            return 1024;
+        }
+
+        @Override
+        public boolean isReQueueOnFail() {
+            return false;
+        }
+
+        @Override
+        public String getConsumerTag() {
+            return "sender";
+        }
+
+        @Override
+        public List<RabbitMQDestination> getQueueName() {
+            List<RabbitMQDestination> list = new ArrayList<RabbitMQDestination>();
+            list.add(new RabbitMQDestination("drone_frame", "drone", "drone_frame"));
+            return list;
+        }
+
+        @Override
+        public MessageBuilder getMessageBuilder() {
+            return new Base64Builder();
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+            outputFieldsDeclarer.declare(new Fields("time1"));
+        }
+
+        @Override
+        public int queueSize() {
+            return 1024;
+        }
+
+        @Override
+        public RabbitMQDestinationSelector getDestinationSelector() {
+            return null;
+        }
+    }
+
+    private static class BoltConfigurator implements RabbitMQConfigurator {
+        private String url = "amqp://localhost:5672";
+
+        @Override
+        public String getURL() {
+            return url;
+        }
+
+        @Override
+        public boolean isAutoAcking() {
+            return true;
+        }
+
+        @Override
+        public int getPrefetchCount() {
+            return 1024;
+        }
+
+        @Override
+        public boolean isReQueueOnFail() {
+            return false;
+        }
+
+        @Override
+        public String getConsumerTag() {
+            return "control";
+        }
+
+        @Override
+        public List<RabbitMQDestination> getQueueName() {
+            List<RabbitMQDestination> list = new ArrayList<RabbitMQDestination>();
+            list.add(new RabbitMQDestination("control", "drone", "control"));
+            return list;
+        }
+
+        @Override
+        public MessageBuilder getMessageBuilder() {
+            return new Base64Builder();
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+            outputFieldsDeclarer.declare(new Fields("control"));
+        }
+
+        @Override
+        public int queueSize() {
+            return 1024;
+        }
+
+        @Override
+        public RabbitMQDestinationSelector getDestinationSelector() {
+            return new RabbitMQDestinationSelector() {
+                @Override
+                public String select(Tuple tuple) {
+                    return "control";
+                }
+            };
+        }
+    }
+}
