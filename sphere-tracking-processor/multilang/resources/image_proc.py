88d0371f8d71bff5ad6a0feb26b4547ec3a7cbe2
oliverlewis <oliverlewis7@gmail.com>
2014-06-20 14:55:35 -0400

    queue

--- sphere-tracking-processor/multilang/resources/image_proc.py
+++ sphere-tracking-processor/multilang/resources/image_proc.py
@@ -8,6 +8,8 @@ import threading
 import base64
 import json
 import numpy as np
+from threading  import Thread
+import Queue
 import ctypes
 from subprocess import PIPE, Popen
 from StringIO import StringIO
@@ -20,46 +22,66 @@ class SplitSentenceBolt(storm.BasicBolt):
         self.p = Popen(["nice", "-n", "15", "avconv", "-i", "-",
                    "-probesize", "2048", "-flags", "low_delay", "-f",
                    "rawvideo", "-pix_fmt", 'rgb24', "-"],
-                  stdin=PIPE, stdout=PIPE, stderr=open('/dev/null', 'w'),
+                  stdin=PIPE, stdout=PIPE, stderr=open('/home/oliver/error', 'w'),
                   bufsize=0, preexec_fn=set_death_signal_int)
+        t = Thread(target=self.enqueue_output, args=(self.p.stdout, (360, 640)))
+        t.daemon = True # thread dies with the program
+        self.q = Queue.Queue()
+        #self.f = open('test.out', 'w')
+
+
+        t.start()
 #        self.decoder = h264decoder.H264Decoder((360, 640, 3))
 
     def process(self, tup):
-        base64Frame = [elem.encode("hex") for elem in tup.values[0]]
+        #base64Frame = [elem.encode("hex") for elem in tup.values[0]]
 
         frame =  base64.b64decode(tup.values[0])
         frame_bytes = [elem.encode("hex") for elem in frame]
         self.p.stdin.write(frame)
-        frame_size=(360, 640)
-        frame_size_bytes = frame_size[0] * frame_size[1] * 3
-        storm.emit(["section1"])
-        buffer_str = self.p.stdout.read(frame_size_bytes)
-        storm.emit(["section2"])
-        im = np.frombuffer(buffer_str, count=frame_size_bytes, dtype=np.uint8)
-        im = im.reshape((frame_size[0], frame_size[1], 3))
-        storm.emit(["section3"])
-        circles = self.trackingSensorModule.detectCircles(im)
-        storm.emit(["section4"])
-        message = {}
+        list = []
+        #while not self.q.empty():
+        if not self.q.empty():
+            list.append(self.q.get())
+            storm.emit(list)
 
-        if circles is not None:
-            if circles.size == 3:
-                position = self.trackingSensorModule.calculatePosition(circles)
-                message["control"] = {"position" : [position[0], position[1]]}
+    def enqueue_output(self, out, frame_size):
+        frame_size_bytes = frame_size[0] * frame_size[1] * 3
+
+        #print "frame: ", frame_size_bytes
+        while True:
+            buffer_str = out.read(frame_size_bytes)
+            #storm.emit(["section4"])
+            im = np.frombuffer(buffer_str, count=frame_size_bytes, dtype=np.uint8)
+            im = im.reshape((frame_size[0], frame_size[1], 3))
+            circles = self.trackingSensorModule.detectCircles(im)
+
+            message = {}
+
+            if circles is not None:
+                if circles.size == 3:
+                    position = self.trackingSensorModule.calculatePosition(circles)
+                    message["control"] = {"position" : [position[0], position[1]]}
+                else:
+                    message["control"] = {"hover" : "true"}
             else:
                 message["control"] = {"hover" : "true"}
-        else:
-            message["control"] = {"hover" : tup.values[0]}
 
-        io = StringIO()
-        json.dump(message, io)
-        storm.emit([io.getvalue()])
+
+            io = StringIO()
+            json.dump(message, io)
+            self.q.put(io.getvalue())
+            #storm.emit([io.getvalue()])
+
 
 # Logic for making ffmpeg terminate on the death of this process
 def set_death_signal(signal):
     libc = ctypes.CDLL('libc.so.6')
     PR_SET_DEATHSIG = 1
     libc.prctl(PR_SET_DEATHSIG, signal)
+
+
+
 
 
 def set_death_signal_int():
