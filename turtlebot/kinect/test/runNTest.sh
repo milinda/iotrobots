8f4a00e8b5b38e22913f7811373a5ce45860e7fd
supunkamburugamuva <supun06@gmail.com>
2014-07-30 15:21:23 -0400

    using java to call the decoder

--- turtlebot/kinect/test/runNTest.sh
+++ turtlebot/kinect/test/runNTest.sh
@@ -3,14 +3,10 @@
 
 javac -classpath ../lib/rabbitmq-client.jar:../lib/freenect-jna.jar:../lib/jna-4.1.0.jar:../lib/jzlib.jar ../src/main/java/*.java -d .
 
-echo "Select an ip address"
-read ipAddr
-
 # run SendFrame.java in a new terminal 
 #(x-terminal... command doesn't work on all machines)
-x-terminal-emulator -e java -cp .:../lib/commons-io-1.2.jar:../lib/commons-cli-1.1.jar:../lib/rabbitmq-client.jar:../lib/jna-4.1.0.jar:../lib/freenect-jna.jar:../lib/jzlib.jar SendFrame_new $ipAddr 
+java -cp .:../lib/commons-io-1.2.jar:../lib/commons-cli-1.1.jar:../lib/rabbitmq-client.jar:../lib/jna-4.1.0.jar:../lib/freenect-jna.jar:../lib/jzlib.jar SendFrame_new $ipAddr $@
 
-java -cp .:../lib/commons-io-1.2.jar:../lib/commons-cli-1.1.jar:../lib/rabbitmq-client.jar:../lib/jna-4.1.0.jar:../lib/freenect-jna.jar:../lib/jzlib.jar RecvFrame_new 
 
 
 
