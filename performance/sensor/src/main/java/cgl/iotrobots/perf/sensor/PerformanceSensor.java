bdbf8e6d25c6ebb80625737dcc5886165d654dfc
supunkamburugamuva <supun06@gmail.com>
2014-09-02 13:57:26 -0400

    fixing some names and adding configurations

--- performance/sensor/src/main/java/cgl/iotrobots/perf/sensor/PerformanceSensor.java
+++ performance/sensor/src/main/java/cgl/iotrobots/perf/sensor/PerformanceSensor.java
@@ -5,11 +5,11 @@ import cgl.iotcloud.core.msg.MessageContext;
 import cgl.iotcloud.core.sensorsite.SiteContext;
 import cgl.iotcloud.core.transport.Channel;
 import cgl.iotcloud.core.transport.Direction;
+import cgl.iotcloud.core.transport.TransportConstants;
 import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,22 +17,22 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class PerformanceSensor extends AbstractSensor {
-    public static final String TURTLE_STORM_FRAMES_ROUTING_KEY = "turtle_storm_frames";
-    public static final String TURTLE_STORM_FRAMES_QUEUE_NAME = "turtle_storm_frames";
-    public static final String TURTLE_STORM_CONTROL_QUEUE_NAME = "turtle_storm_control";
-    public static final String TURTLE_STORM_CONTROL_ROUTING_KEY = "turtle_storm_control";
-    public static final String TURTLE_EXCHANGE = "turtle";
-    public static final String CMD_RECV = "cmd_recv";
-    public static final String FRAME_SENDER = "frame_sender";
-
     private static Logger LOG = LoggerFactory.getLogger(PerformanceSensor.class);
 
-    public static final String BROKER_URL = "broker_url";
+    public static final String PERF_SEND_DATA_ROUTING_KEY = "perf_send_data";
+    public static final String PERF_SEND_DATA_QUEUE_NAME = "perf_send_data";
+    public static final String PERF_RECV_QUEUE_NAME = "perf_recv_data";
+    public static final String PERF_RECV_ROUTING_KEY = "perf_recv_data";
+    public static final String DATA_SENDER = "data_sender";
+    public static final String DATA_RECEIVER = "data_receiver";
+    private static final String PERF_EXCHANGE = "perf";
+
 
     public static final String MODE_ARG = "mode";
-    public static final String URL_ARG = "url";
-    public static final String LOCAL_IP_ARG = "local_ip";
-    public static final String ROS_MASTER_ARG = "ros_master";
+    public static final String TRP_ARG = "trp";
+    public static final String DATA_SIZE_ARG = "data";
+    public static final String DATA_INTERVAL = "freq";
+    public static final String
 
     private boolean run = true;
 
@@ -50,9 +50,8 @@ public class PerformanceSensor extends AbstractSensor {
 
     @Override
     public void open(SensorContext context) {
-        final BlockingQueue receivingQueue = new LinkedBlockingQueue();
-        final Channel sendChannel = context.getChannel("rabbitmq", FRAME_SENDER);
-        final Channel receiveChannel = context.getChannel("rabbitmq", CMD_RECV);
+        final Channel sendChannel = context.getChannel(TransportConstants.TRANSPORT_RABBITMQ, DATA_SENDER);
+        final Channel receiveChannel = context.getChannel(TransportConstants.TRANSPORT_RABBITMQ, DATA_RECEIVER);
 
         // startSend(sendChannel, receivingQueue);
         Thread t = new Thread(new Runnable() {
@@ -60,7 +59,6 @@ public class PerformanceSensor extends AbstractSensor {
             public void run() {
                 while (run) {
                     try {
-                        byte[] body = (byte[]) receivingQueue.take();
                         Map<String, Object> props = new HashMap<String, Object>();
                         props.put("time", Long.toString(System.currentTimeMillis()));
 
@@ -97,31 +95,25 @@ public class PerformanceSensor extends AbstractSensor {
     private class STSensorConfigurator extends AbstractConfigurator {
         @Override
         public SensorContext configure(SiteContext siteContext, Map conf) {
-            String brokerUrl = (String) conf.get(BROKER_URL);
-            String rosMaster = (String) conf.get(ROS_MASTER_ARG);
-            String localIp = (String) conf.get(LOCAL_IP_ARG);
             String mode = (String) conf.get(MODE_ARG);
 
             SensorContext context = new SensorContext("turtle_sensor");
-            context.addProperty(BROKER_URL, brokerUrl);
-            context.addProperty(ROS_MASTER_ARG, rosMaster);
-            context.addProperty(LOCAL_IP_ARG, localIp);
             context.addProperty(MODE_ARG, mode);
 
             Map sendProps = new HashMap();
-            sendProps.put("exchange", TURTLE_EXCHANGE);
-            sendProps.put("routingKey", TURTLE_STORM_FRAMES_ROUTING_KEY);
-            sendProps.put("queueName", TURTLE_STORM_FRAMES_QUEUE_NAME);
-            Channel sendChannel = createChannel(FRAME_SENDER, sendProps, Direction.OUT, 1024);
+            sendProps.put("exchange", PERF_EXCHANGE);
+            sendProps.put("routingKey", PERF_SEND_DATA_ROUTING_KEY);
+            sendProps.put("queueName", PERF_SEND_DATA_QUEUE_NAME);
+            Channel sendChannel = createChannel(DATA_SENDER, sendProps, Direction.OUT, 1024);
 
             Map receiveProps = new HashMap();
-            receiveProps.put("queueName", TURTLE_STORM_CONTROL_QUEUE_NAME);
-            receiveProps.put("exchange", TURTLE_EXCHANGE);
-            receiveProps.put("routingKey", TURTLE_STORM_CONTROL_ROUTING_KEY);
-            Channel receiveChannel = createChannel(CMD_RECV, receiveProps, Direction.IN, 1024);
+            receiveProps.put("queueName", PERF_RECV_QUEUE_NAME);
+            receiveProps.put("exchange", PERF_EXCHANGE);
+            receiveProps.put("routingKey", PERF_RECV_ROUTING_KEY);
+            Channel receiveChannel = createChannel(DATA_RECEIVER, receiveProps, Direction.IN, 1024);
 
-            context.addChannel("rabbitmq", sendChannel);
-            context.addChannel("rabbitmq", receiveChannel);
+            context.addChannel(TransportConstants.TRANSPORT_RABBITMQ, sendChannel);
+            context.addChannel(TransportConstants.TRANSPORT_RABBITMQ, receiveChannel);
 
             return context;
         }
@@ -130,28 +122,16 @@ public class PerformanceSensor extends AbstractSensor {
     private static Map<String, String> getProperties(String []args) {
         Map<String, String> conf = new HashMap<String, String>();
         Options options = new Options();
-        options.addOption(URL_ARG, true, "URL of the AMQP Broker");
-        options.addOption(ROS_MASTER_ARG, true, "Ros master URL");
-        options.addOption(LOCAL_IP_ARG, true, "Local IP address");
         options.addOption(MODE_ARG, true, "possible options are (nt, t) nt means without connecting to turtle");
-
+        options.addOption(TRP_ARG, true, "k or r");
         CommandLineParser commandLineParser = new BasicParser();
         try {
             CommandLine cmd = commandLineParser.parse(options, args);
-
-            String url = cmd.getOptionValue(URL_ARG);
-            String rosMaster = cmd.getOptionValue(ROS_MASTER_ARG);
-            String localIp = cmd.getOptionValue(LOCAL_IP_ARG);
             String mode = cmd.getOptionValue(MODE_ARG);
+            String trp = cmd.getOptionValue(TRP_ARG);
 
-            System.out.println(url);
-            System.out.println(rosMaster);
-            System.out.println(localIp);
-
-            conf.put(BROKER_URL, url);
-            conf.put(ROS_MASTER_ARG, rosMaster);
-            conf.put(LOCAL_IP_ARG, localIp);
             conf.put(MODE_ARG, mode);
+            conf.put(TRP_ARG, trp);
 
             return conf;
         } catch (ParseException e) {
