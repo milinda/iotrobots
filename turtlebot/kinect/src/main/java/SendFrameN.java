3b7628fb24eebaa1d8ca2e17258de2f2999bc7db
supunkamburugamuva <supun06@gmail.com>
2014-09-20 16:07:34 -0400

    git adding more functionality

--- turtlebot/kinect/src/main/java/SendFrameN.java
+++ turtlebot/kinect/src/main/java/SendFrameN.java
@@ -81,7 +81,7 @@ public class SendFrameN {
             long time = System.currentTimeMillis();
             byte[] data = compress(buffer, inverted);
             send(channel, exchange_name, data, time);
-            Thread.sleep(20);
+            Thread.sleep(35);
         }
     }
 
