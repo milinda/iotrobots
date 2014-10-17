c897195207381af4a87fd191901b78c2ec560ec1
supunkamburugamuva <supun06@gmail.com>
2014-10-17 15:51:08 -0400

    git adding more functionality

--- slam/src/main/java/cgl/iotrobots/slam/core/gridfastsalm/GridSlamProcessor.java
+++ slam/src/main/java/cgl/iotrobots/slam/core/gridfastsalm/GridSlamProcessor.java
@@ -1,5 +1,6 @@
 package cgl.iotrobots.slam.core.gridfastsalm;
 
+import cgl.iotrobots.slam.core.scanmatcher.ScanMatcher;
 import cgl.iotrobots.slam.core.utils.OrientedPoint;
 
 import java.util.ArrayList;
@@ -10,12 +11,28 @@ import java.util.concurrent.LinkedBlockingDeque;
 public class GridSlamProcessor {
     private List<TNode> TNodeVector = new ArrayList<TNode>();
     private BlockingDeque<TNode> TNodeDeque = new LinkedBlockingDeque<TNode>();
-    private List<Particle> ParticleVector = new ArrayList<Particle>();
+    private List<Particle> m_particles = new ArrayList<Particle>();
 
+    ScanMatcher m_matcher;
 
-    double srr;
-    double srt;
-    double str;
-    double stt;
+    MotionModel m_motionModel;
+
+    int m_beams;
+    double last_update_time_;
+    double period_;
+
     double minimumScore;
+
+    double resampleThreshold;
+
+    int  m_count, m_readingCount;
+    OrientedPoint m_lastPartPose;
+    OrientedPoint m_odoPose;
+    OrientedPoint m_pose;
+
+    double m_linearDistance, m_angularDistance;
+
+    double neff;
+
+    double xmin;
 }
