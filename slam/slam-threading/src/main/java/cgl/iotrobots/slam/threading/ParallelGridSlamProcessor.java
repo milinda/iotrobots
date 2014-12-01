6b97359e61e1f618b5284dc66bad6e5a44fb40ba
supunkamburugamuva <supun06@gmail.com>
2014-12-01 16:38:33 -0500

    adding thread based parallel code

--- slam/slam-threading/src/main/java/cgl/iotrobots/slam/threading/ParallelGridSlamProcessor.java
+++ slam/slam-threading/src/main/java/cgl/iotrobots/slam/threading/ParallelGridSlamProcessor.java
@@ -3,6 +3,9 @@ package cgl.iotrobots.slam.threading;
 import cgl.iotrobots.slam.core.gridfastsalm.AbstractGridSlamProcessor;
 
 public class ParallelGridSlamProcessor extends AbstractGridSlamProcessor {
+    private int parallelism;
+
+
 
     @Override
     public void scanMatch(double[] plainReading) {
