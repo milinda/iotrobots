0941d788d0a72d652e13d7b06c8ef305707692bf
supunkamburugamuva <supun06@gmail.com>
2014-12-01 16:21:54 -0500

    refactoring code

--- slam/slam-threading/src/main/java/cgl/iotrobots/slam/threading/ParallelGridSlamProcessor.java
+++ slam/slam-threading/src/main/java/cgl/iotrobots/slam/threading/ParallelGridSlamProcessor.java
@@ -4,4 +4,8 @@ import cgl.iotrobots.slam.core.gridfastsalm.AbstractGridSlamProcessor;
 
 public class ParallelGridSlamProcessor extends AbstractGridSlamProcessor {
 
+    @Override
+    public void scanMatch(double[] plainReading) {
+
+    }
 }
