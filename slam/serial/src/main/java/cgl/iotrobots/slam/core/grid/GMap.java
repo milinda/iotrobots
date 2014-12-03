81f1104f62799c754cbe18541e5ceb21493a7075
supunkamburugamuva <supun06@gmail.com>
2014-12-03 09:55:45 -0500

    refactoring

--- slam/serial/src/main/java/cgl/iotrobots/slam/core/grid/GMap.java
+++ slam/serial/src/main/java/cgl/iotrobots/slam/core/grid/GMap.java
@@ -15,16 +15,16 @@ public class GMap {
 
     public GMap(int mapSizeX, int mapSizeY, double delta) {
         m_storage = new HierarchicalArray2D(mapSizeX, mapSizeY, DEFAULT_PATCH);
-        m_worldSizeX=mapSizeX * delta;
-        m_worldSizeY=mapSizeY * delta;
-        m_delta=delta;
-        m_center= new DoublePoint(0.5*m_worldSizeX, 0.5*m_worldSizeY);
-        m_sizeX2=m_mapSizeX>>1;
-        m_sizeY2=m_mapSizeY>>1;
+        m_worldSizeX = mapSizeX * delta;
+        m_worldSizeY = mapSizeY * delta;
+        m_delta = delta;
+        m_center = new DoublePoint(0.5 * m_worldSizeX, 0.5 * m_worldSizeY);
+        m_sizeX2 = m_mapSizeX >> 1;
+        m_sizeY2 = m_mapSizeY >> 1;
     }
 
     public GMap(DoublePoint center, double worldSizeX, double worldSizeY, double delta) {
-        m_storage = new HierarchicalArray2D(new Double(Math.ceil(worldSizeX/delta)).intValue(), new Double(Math.ceil(worldSizeY/delta)).intValue(), DEFAULT_PATCH);
+        m_storage = new HierarchicalArray2D(new Double(Math.ceil(worldSizeX / delta)).intValue(), new Double(Math.ceil(worldSizeY / delta)).intValue(), DEFAULT_PATCH);
         m_center = center;
         m_worldSizeX = worldSizeX;
         m_worldSizeY = worldSizeY;
@@ -36,7 +36,7 @@ public class GMap {
     }
 
     public GMap(DoublePoint center, double xmin, double ymin, double xmax, double ymax, double delta) {
-        m_storage = new HierarchicalArray2D(new Double(Math.ceil((xmax-xmin)/delta)).intValue(),new Double(Math.ceil((ymax-ymin)/delta)).intValue(), DEFAULT_PATCH);
+        m_storage = new HierarchicalArray2D(new Double(Math.ceil((xmax - xmin) / delta)).intValue(), new Double(Math.ceil((ymax - ymin) / delta)).intValue(), DEFAULT_PATCH);
         m_center = center;
         m_worldSizeX = xmax - xmin;
         m_worldSizeY = ymax - ymin;
@@ -47,21 +47,44 @@ public class GMap {
         m_sizeY2 = (int) Math.round((m_center.y - ymin) / m_delta);
     }
 
-    public DoublePoint getCenter() {return m_center;}
-    public double getWorldSizeX() {return m_worldSizeX;}
-    public double getWorldSizeY() {return m_worldSizeY;}
-    public int getMapSizeX() {return m_mapSizeX;}
-    public int getMapSizeY() {return m_mapSizeY;}
-    public double getDelta() { return m_delta;}
-    public double getMapResolution() { return m_delta;}
-    public double getResolution() { return m_delta;}
+    public DoublePoint getCenter() {
+        return m_center;
+    }
+
+    public double getWorldSizeX() {
+        return m_worldSizeX;
+    }
+
+    public double getWorldSizeY() {
+        return m_worldSizeY;
+    }
+
+    public int getMapSizeX() {
+        return m_mapSizeX;
+    }
+
+    public int getMapSizeY() {
+        return m_mapSizeY;
+    }
+
+    public double getDelta() {
+        return m_delta;
+    }
+
+    public double getMapResolution() {
+        return m_delta;
+    }
+
+    public double getResolution() {
+        return m_delta;
+    }
 
     public HierarchicalArray2D getStorage() {
         return m_storage;
     }
 
     public Size getSize(double xmin, double ymin, double xmax, double ymax) {
-        DoublePoint min=map2world(new IntPoint(0,0)), max=map2world(new IntPoint(m_mapSizeX-1, m_mapSizeY-1));
+        DoublePoint min = map2world(new IntPoint(0, 0)), max = map2world(new IntPoint(m_mapSizeX - 1, m_mapSizeY - 1));
         return new Size(min.x, min.y, max.x, max.y);
     }
 
@@ -74,7 +97,7 @@ public class GMap {
     }
 
     public boolean isInside(int x, int y) {
-        return (m_storage.cellState(new IntPoint(x,y)) & AccessibilityState.Inside.getVal()) > 0;
+        return (m_storage.cellState(new IntPoint(x, y)) & AccessibilityState.Inside.getVal()) > 0;
     }
 
     public boolean isInside(IntPoint p) {
@@ -85,70 +108,70 @@ public class GMap {
         return isInside(p.x, p.y);
     }
 
-    public boolean isInside(double x, double y)  {
+    public boolean isInside(double x, double y) {
         return m_storage.cellState(
-                world2map(new IntPoint(new Double(x).intValue(),new Double(y).intValue())))
+                world2map(new IntPoint(new Double(x).intValue(), new Double(y).intValue())))
                 == AccessibilityState.Inside.getVal();
     }
 
 
-    public void resize(double xmin, double ymin, double xmax, double ymax){
+    public void resize(double xmin, double ymin, double xmax, double ymax) {
         IntPoint imin = world2map(xmin, ymin);
-        IntPoint imax=world2map(xmax, ymax);
+        IntPoint imax = world2map(xmax, ymax);
         int pxmin, pymin, pxmax, pymax;
-        pxmin=(int)Math.floor((double) imin.x / (1 << m_storage.getPatchMagnitude()));
-        pxmax=(int)Math.ceil((double) imax.x / (1 << m_storage.getPatchMagnitude()));
-        pymin=(int)Math.floor((double) imin.y / (1 << m_storage.getPatchMagnitude()));
-        pymax=(int)Math.ceil((double) imax.y / (1 << m_storage.getPatchMagnitude()));
+        pxmin = (int) Math.floor((double) imin.x / (1 << m_storage.getPatchMagnitude()));
+        pxmax = (int) Math.ceil((double) imax.x / (1 << m_storage.getPatchMagnitude()));
+        pymin = (int) Math.floor((double) imin.y / (1 << m_storage.getPatchMagnitude()));
+        pymax = (int) Math.ceil((double) imax.y / (1 << m_storage.getPatchMagnitude()));
         m_storage.resize(pxmin, pymin, pxmax, pymax);
-        m_mapSizeX=m_storage.getXSize()<<m_storage.getPatchSize();
-        m_mapSizeY=m_storage.getYSize()<<m_storage.getPatchSize();
-        m_worldSizeX=xmax-xmin;
-        m_worldSizeY=ymax-ymin;
-        m_sizeX2-=pxmin*(1<<m_storage.getPatchMagnitude());
-        m_sizeY2-=pymin*(1<<m_storage.getPatchMagnitude());
+        m_mapSizeX = m_storage.getXSize() << m_storage.getPatchSize();
+        m_mapSizeY = m_storage.getYSize() << m_storage.getPatchSize();
+        m_worldSizeX = xmax - xmin;
+        m_worldSizeY = ymax - ymin;
+        m_sizeX2 -= pxmin * (1 << m_storage.getPatchMagnitude());
+        m_sizeY2 -= pymin * (1 << m_storage.getPatchMagnitude());
     }
 
-    public void grow(double xmin, double ymin, double xmax, double ymax){
-        IntPoint imin=world2map(xmin, ymin);
-        IntPoint imax=world2map(xmax, ymax);
+    public void grow(double xmin, double ymin, double xmax, double ymax) {
+        IntPoint imin = world2map(xmin, ymin);
+        IntPoint imax = world2map(xmax, ymax);
         if (isInside(imin) && isInside(imax))
             return;
-        imin= IntPoint.min(imin, new IntPoint(0, 0));
-        imax= IntPoint.max(imax, new IntPoint(m_mapSizeX - 1, m_mapSizeY - 1));
+        imin = IntPoint.min(imin, new IntPoint(0, 0));
+        imax = IntPoint.max(imax, new IntPoint(m_mapSizeX - 1, m_mapSizeY - 1));
         int pxmin, pymin, pxmax, pymax;
-        pxmin=(int)Math.floor((double) imin.x / (1 << m_storage.getPatchMagnitude()));
-        pxmax=(int)Math.ceil((double) imax.x / (1 << m_storage.getPatchMagnitude()));
-        pymin=(int)Math.floor((double) imin.y / (1 << m_storage.getPatchMagnitude()));
-        pymax=(int)Math.ceil((double) imax.y / (1 << m_storage.getPatchMagnitude()));
+        pxmin = (int) Math.floor((double) imin.x / (1 << m_storage.getPatchMagnitude()));
+        pxmax = (int) Math.ceil((double) imax.x / (1 << m_storage.getPatchMagnitude()));
+        pymin = (int) Math.floor((double) imin.y / (1 << m_storage.getPatchMagnitude()));
+        pymax = (int) Math.ceil((double) imax.y / (1 << m_storage.getPatchMagnitude()));
         m_storage.resize(pxmin, pymin, pxmax, pymax);
-        m_mapSizeX=m_storage.getXSize()<<m_storage.getPatchSize();
-        m_mapSizeY=m_storage.getYSize()<<m_storage.getPatchSize();
-        m_worldSizeX=xmax-xmin;
-        m_worldSizeY=ymax-ymin;
-        m_sizeX2-=pxmin*(1<<m_storage.getPatchMagnitude());
-        m_sizeY2-=pymin*(1<<m_storage.getPatchMagnitude());
+        m_mapSizeX = m_storage.getXSize() << m_storage.getPatchSize();
+        m_mapSizeY = m_storage.getYSize() << m_storage.getPatchSize();
+        m_worldSizeX = xmax - xmin;
+        m_worldSizeY = ymax - ymin;
+        m_sizeX2 -= pxmin * (1 << m_storage.getPatchMagnitude());
+        m_sizeY2 -= pymin * (1 << m_storage.getPatchMagnitude());
     }
 
     public IntPoint world2map(double x, double y) {
-        return new IntPoint((int)Math.round((x-m_center.x)/m_delta)+m_sizeX2, (int)Math.round((y-m_center.y)/m_delta)+m_sizeY2);
+        return new IntPoint((int) Math.round((x - m_center.x) / m_delta) + m_sizeX2, (int) Math.round((y - m_center.y) / m_delta) + m_sizeY2);
     }
 
     public IntPoint world2map(IntPoint p) {
-        return new IntPoint((int) Math.round(((Integer)p.x - m_center.x) / m_delta) + m_sizeX2, (int) Math.round(((Integer)p.y - m_center.y) / m_delta) + m_sizeY2);
+        return new IntPoint((int) Math.round(((Integer) p.x - m_center.x) / m_delta) + m_sizeX2, (int) Math.round(((Integer) p.y - m_center.y) / m_delta) + m_sizeY2);
     }
 
     public IntPoint world2map(DoublePoint p) {
-        return new IntPoint((int) Math.round(((Double)p.x - m_center.x) / m_delta) + m_sizeX2, (int) Math.round(((Double)p.y - m_center.y) / m_delta) + m_sizeY2);
+        return new IntPoint((int) Math.round(((Double) p.x - m_center.x) / m_delta) + m_sizeX2, (int) Math.round(((Double) p.y - m_center.y) / m_delta) + m_sizeY2);
     }
 
     public IntPoint world2map(DoubleOrientedPoint p) {
-        return new IntPoint((int) Math.round(((Double)p.x - m_center.x) / m_delta) + m_sizeX2, (int) Math.round(((Double)p.y - m_center.y) / m_delta) + m_sizeY2);
+        return new IntPoint((int) Math.round(((Double) p.x - m_center.x) / m_delta) + m_sizeX2, (int) Math.round(((Double) p.y - m_center.y) / m_delta) + m_sizeY2);
     }
 
     public DoublePoint map2world(IntPoint p) {
-        return new DoublePoint((p.x-m_sizeX2)*m_delta + m_center.x,
-                (p.y-m_sizeY2)*m_delta + m_center.y);
+        return new DoublePoint((p.x - m_sizeX2) * m_delta + m_center.x,
+                (p.y - m_sizeY2) * m_delta + m_center.y);
     }
 
     public Object cell(IntPoint p, boolean c) {
