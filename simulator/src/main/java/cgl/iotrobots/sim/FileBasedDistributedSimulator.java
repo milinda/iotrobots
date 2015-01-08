f82b6d0df78880138b3b7572ad81ba4c13fac3da
supunkamburugamuva <supun06@gmail.com>
2015-01-08 11:42:12 -0500

    running with 4 bolts

--- simulator/src/main/java/cgl/iotrobots/sim/FileBasedDistributedSimulator.java
+++ simulator/src/main/java/cgl/iotrobots/sim/FileBasedDistributedSimulator.java
@@ -17,10 +17,10 @@ public class FileBasedDistributedSimulator {
     RabbitMQSender sender;
     RabbitMQReceiver receiver;
     RabbitMQReceiver bestReceiver;
-//    private String url = "amqp://localhost:5672";
+    private String url = "amqp://localhost:5672";
     BufferedReader br = null;
     Kryo kryo = new Kryo();
-    private String url = "amqp://149.165.159.3:5672";
+//    private String url = "amqp://149.165.159.3:5672";
     public FileBasedDistributedSimulator() {
         try {
             sender = new RabbitMQSender(url, "simbard_laser");
