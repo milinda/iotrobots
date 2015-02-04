b8aba4d23f628ee42ef0ae02cf580bc70f174f70
supunkamburugamuva <supun06@gmail.com>
2015-02-04 14:45:40 -0500

    fixing angle calculation

--- simulator/src/main/java/cgl/iotrobots/sim/TurtleSimulator.java
+++ simulator/src/main/java/cgl/iotrobots/sim/TurtleSimulator.java
@@ -108,6 +108,7 @@ public class TurtleSimulator {
                 public void onNewMessage(Odometry odometry) {
                     double rad = quantarianToRad(odometry.getPose().getPose().getOrientation());
                     if (state == State.WAITING_LASER_SCAN) {
+                        System.out.format("received odometry: x = %f, y = %f, theta = %f\n", odometry.getPose().getPose().getPosition().getX(), odometry.getPose().getPose().getPosition().getY(), rad);
                         lastPose = new DoubleOrientedPoint(odometry.getPose().getPose().getPosition().getX(),
                                 odometry.getPose().getPose().getPosition().getY(), rad);
                     }
@@ -163,7 +164,9 @@ public class TurtleSimulator {
     }
 
     private double quantarianToRad(Quaternion q) {
-        return Math.atan2(2.0 * (q.getY() * q.getZ() + q.getW() * q.getX()), q.getW() * q.getW() - q.getX() * q.getX() - q.getY() * q.getY() + q.getZ() * q.getZ());
+        //return Math.atan2(2.0 * (q.getY() * q.getZ() + q.getW() * q.getX()), q.getW() * q.getW() - q.getX() * q.getX() - q.getY() * q.getY() + q.getZ() * q.getZ());
+//        return Math.atan2(2.0 * (q.getX() * q.getZ() + q.getY() * q.getW()), 1 - 2 * (q.getZ() * q.getZ() + q.getW() * q.getW()));
+        return new Matrix3(q).getEulerYPR().yaw;
     }
 
     private class Worker implements Runnable {
@@ -221,4 +224,102 @@ public class TurtleSimulator {
     private DoubleOrientedPoint getOdomPose(long t) {
         return null;
     }
+
+    private class Matrix3 {
+        double [][]m_el = new double[3][3];
+
+        private Matrix3(Quaternion q) {
+            setRotation(q);
+        }
+
+        void setRotation(Quaternion q) {
+            double d = 2;
+            double s = 2.0 / d;
+            double xs = q.getX() * s,   ys = q.getY() * s,   zs = q.getZ() * s;
+            double wx = q.getW() * xs,  wy = q.getW() * ys,  wz = q.getW() * zs;
+            double xx = q.getX() * xs,  xy = q.getX() * ys,  xz = q.getX() * zs;
+            double yy = q.getY() * ys,  yz = q.getY() * zs,  zz = q.getZ() * zs;
+
+            setValue((1.0) - (yy + zz), xy - wz, xz + wy,
+                    xy + wz, (1.0) - (xx + zz), yz - wx,
+                    xz - wy, yz + wx, (1.0) - (xx + yy));
+        }
+
+        void setValue(double xx, double xy, double xz,
+                      double yx, double yy, double yz,
+                      double zx, double zy, double zz) {
+            m_el[0][0] = xx;
+            m_el[0][1] = xy;
+            m_el[0][2] = xz;
+            m_el[1][0] = yx;
+            m_el[1][1] = yy;
+            m_el[1][2] = yz;
+            m_el[2][0] = zx;
+            m_el[2][1] = zy;
+            m_el[2][2] = zz;
+        }
+
+        Euler getEulerYPR()
+        {
+            Euler euler_out = new Euler();
+            Euler euler_out2 = new Euler(); //second solution
+            //get the pointer to the raw data
+
+            // Check that pitch is not at a singularity
+            // Check that pitch is not at a singularity
+            if (Math.abs(m_el[2][0]) >= 1)
+            {
+                euler_out.yaw = 0;
+                euler_out2.yaw = 0;
+
+                // From difference of angles formula
+                if (m_el[2][0] < 0)  //gimbal locked down
+                {
+                    double delta = Math.atan2(m_el[0][1],m_el[0][2]);
+                    euler_out.pitch = Math.PI / 2.0;
+                    euler_out2.pitch = Math.PI / 2.0;
+                    euler_out.roll = delta;
+                    euler_out2.roll = delta;
+                }
+                else {
+                    double delta = Math.atan2(-m_el[0][1],-m_el[0][2]);
+                    euler_out.pitch = -Math.PI / 2.0;
+                    euler_out2.pitch = -Math.PI / 2.0;
+                    euler_out.roll = delta;
+                    euler_out2.roll = delta;
+                }
+            }
+            else
+            {
+                euler_out.pitch = - Math.asin(m_el[2][0]);
+                euler_out2.pitch = Math.PI - euler_out.pitch;
+
+                euler_out.roll = Math.atan2(m_el[2][1]/Math.cos(euler_out.pitch),
+                        m_el[2][2]/Math.cos(euler_out.pitch));
+                euler_out2.roll = Math.atan2(m_el[2][1]/Math.cos(euler_out2.pitch),
+                        m_el[2][2]/Math.cos(euler_out2.pitch));
+
+                euler_out.yaw = Math.atan2(m_el[1][0]/Math.cos(euler_out.pitch),
+                        m_el[0][0]/Math.cos(euler_out.pitch));
+                euler_out2.yaw = Math.atan2(m_el[1][0]/Math.cos(euler_out2.pitch),
+                        m_el[0][0]/Math.cos(euler_out2.pitch));
+            }
+
+            Euler e = new Euler();
+
+            e.yaw = euler_out.yaw;
+            e.pitch = euler_out.pitch;
+            e.roll = euler_out.roll;
+
+            return e;
+        }
+    }
+
+    class Euler {
+        double yaw;
+        double pitch;
+        double roll;
+    }
+
+
 }
