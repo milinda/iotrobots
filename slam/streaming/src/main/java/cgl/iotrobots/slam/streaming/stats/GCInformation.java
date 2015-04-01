fb73153ac38cd2deec27257c98897239f5b17378
supunkamburugamuva <supun06@gmail.com>
2015-04-01 11:38:43 -0400

    fixing an error with dispatcher time calculation

--- slam/streaming/src/main/java/cgl/iotrobots/slam/streaming/stats/GCInformation.java
+++ slam/streaming/src/main/java/cgl/iotrobots/slam/streaming/stats/GCInformation.java
@@ -62,14 +62,10 @@ public class GCInformation {
         LOG.info("gcbean size()....................................." + gcbeans.size());
         //Install a notifcation handler for each bean
         for (java.lang.management.GarbageCollectorMXBean gcbean : gcbeans) {
-//            System.out.println(gcbean);
             NotificationEmitter emitter = (NotificationEmitter) gcbean;
             //use an anonymously generated listener for this example
             // - proper code should really use a named class
             NotificationListener listener = new NotificationListener() {
-                //keep a count of the total time spent in GCs
-//                long totalGcDuration = 0;
-
                 //implement the notifier callback handler
                 @Override
                 public void handleNotification(Notification notification, Object handback) {
@@ -85,38 +81,16 @@ public class GCInformation {
                                 GCCounter counter = e.getValue();
                                 String gctype = info.getGcAction();
                                 if ("end of minor GC".equals(gctype)) {
-                                    gctype = "Young Gen GC";
                                     counter.addYoungGCTime(duration);
                                 } else if ("end of major GC".equals(gctype)) {
-                                    gctype = "Old Gen GC";
                                     counter.addFullGCTime(duration);
+                                } else {
+                                    counter.addYoungGCTime(duration);
                                 }
-//                            System.out.println();
-//                            System.out.println(gctype + ": - " + info.getGcInfo().getId()+ " " + info.getGcName() + " (from " + info.getGcCause()+") "+duration + " microseconds; start-end times " + info.getGcInfo().getStartTime()+ "-" + info.getGcInfo().getEndTime());
                             }
                         } finally {
                             lock.unlock();
                         }
-//                        //Get the information about each memory space, and pretty print it
-//                        Map<String, MemoryUsage> membefore = info.getGcInfo().getMemoryUsageBeforeGc();
-//                        Map<String, MemoryUsage> mem = info.getGcInfo().getMemoryUsageAfterGc();
-//                        for (Map.Entry<String, MemoryUsage> entry : mem.entrySet()) {
-//                            String name = entry.getKey();
-//                            MemoryUsage memdetail = entry.getValue();
-//                            long memInit = memdetail.getInit();
-//                            long memCommitted = memdetail.getCommitted();
-//                            long memMax = memdetail.getMax();
-//                            long memUsed = memdetail.getUsed();
-//                            MemoryUsage before = membefore.get(name);
-//                            long beforepercent = ((before.getUsed()*1000L)/before.getCommitted());
-//                            long percent = ((memUsed*1000L)/before.getCommitted()); //>100% when it gets expanded
-//
-//                            System.out.print(name + (memCommitted==memMax?"(fully expanded)":"(still expandable)") +"used: "+(beforepercent/10)+"."+(beforepercent%10)+"%->"+(percent/10)+"."+(percent%10)+"%("+((memUsed/1048576)+1)+"MB) / ");
-//                        }
-//                        System.out.println();
-//                        totalGcDuration += info.getGcInfo().getDuration();
-//                        long percent = totalGcDuration*1000L/info.getGcInfo().getEndTime();
-//                        System.out.println("GC cumulated overhead "+(percent/10)+"."+(percent%10)+"%");
                     }
                 }
             };
