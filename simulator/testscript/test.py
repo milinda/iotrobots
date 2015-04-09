5a4e6305545361a24fec5bca1549563a3f7faf9c
Supun <supun06@gmail.com>
2015-04-09 00:46:48 -0400

    removing intermediate object creation

--- simulator/testscript/test.py
+++ simulator/testscript/test.py
@@ -13,6 +13,10 @@ sshI = paramiko.SSHClient()
 sshI.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 sshI.connect('10.39.1.26', username='ubuntu', key_filename='/home/ubuntu/skamburu-key')
 
+sshIR = paramiko.SSHClient()
+sshIR.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+sshIR.connect('10.39.1.26', username='ubuntu', key_filename='/home/ubuntu/skamburu-key')
+
 def exec_storm(ssh, particles, parallel):
     cmd = './bin/storm jar ~/projects/iotrobots/slam/streaming/target/iotrobots-slam-streaming-1.0-SNAPSHOT-jar-with-dependencies.jar cgl.iotrobots.slam.streaming.SLAMTopology -name slam_processor -ds_mode 0 -p ' + str(parallel) + ' -pt ' + str(particles) + ' -i'
     channel = ssh.invoke_shell()
@@ -21,7 +25,7 @@ def exec_storm(ssh, particles, parallel):
     stdin.write('''
     cd deploy/storm
     ./bin/storm kill slam_processor -w 1
-    sleep 10
+    sleep 20
     ''' + cmd + '''
     exit
     ''')
@@ -31,16 +35,69 @@ def exec_storm(ssh, particles, parallel):
     time.sleep(30)
 
 def exec_rabbit(ssh):
-    stdin, stdout, stderr = ssh.exec_command('cd /home/ubuntu/deploy/rabbitmq_server-3.3.2/sbin; sudo ./rabbitmq-server')
+    channel = ssh.invoke_shell()
+    stdin = channel.makefile('wb')
+    stdout = channel.makefile('rb')
+    stdin.write('''
+    pid=`ps ax | grep "rabbitmq" | awk '{print $1}'`
+    sudo kill $pid
+    cd /home/ubuntu/deploy/rabbitmq_server-3.3.2/sbin
+    sudo ./rabbitmq-server
+    exit
+    ''')
+    print stdout.read()
+    stdout.close()
+    stdin.close()
+    time.sleep(30)
 
 def exec_iotcloud(ssh):
-    stdin, stdout, stderr = ssh.exec_command('cd deploy/iotcloud2-bin-1.0-SNAPSHOT; ./bin/iotcloud local')
+    channel = ssh.invoke_shell()
+    stdin = channel.makefile('wb')
+    stdout = channel.makefile('rb')
+    stdin.write('''
+    pid=`jps | grep "LocalCluster" | awk '{print $1}'`
+    kill $pid
+    cd deploy/iotcloud2-bin-1.0-SNAPSHOT
+    ./bin/iotcloud local > /dev/null 2>&1 &
+    exit
+    ''')
+    print stdout.read()
+    stdout.close()
+    stdin.close()
 
 def exec_sensor(ssh):
-    stdin, stdout, stderr = ssh.exec_command('cd deploy/iotcloud2-bin-1.0-SNAPSHOT; ./bin/iotcloud jar repository/sensors/iotrobots-slam-sensor-1.0-SNAPSHOT-jar-with-dependencies.jar cgl.iotrobots.slam.sensor.SlamSensor -s local -sim -url "amqp://10.39.1.28:5672"')
+    channel = ssh.invoke_shell()
+    stdin = channel.makefile('wb')
+    stdout = channel.makefile('rb')
+    stdin.write('''
+    cd deploy/iotcloud2-bin-1.0-SNAPSHOT
+    ./bin/iotcloud jar repository/sensors/iotrobots-slam-sensor-1.0-SNAPSHOT-jar-with-dependencies.jar cgl.iotrobots.slam.sensor.SlamSensor -s local -sim -url "amqp://10.39.1.28:5672
+    exit
+    ''')
+    print stdout.read()
+    stdout.close()
+    stdin.close()
+
+def run_test(ssh, test, parallel, particles, input, simbad):
+    cmd = 'java -cp target/simulator-1.0-SNAPSHOT-jar-with-dependencies.jar cgl.iotrobots.sim.FileBasedDistributedSimulator "amqp://10.39.1.28:5672" ' + str(input) + ' results_dir/' +str(test) + str(particles) + '_' + str(parallel) + ' ' +str(simbad) + ' false 1000'
+    channel = ssh.invoke_shell()
+    stdin = channel.makefile('wb')
+    stdout = channel.makefile('rb')
+    stdin.write('''
+    cd /home/ubuntu/projects/iotrobots/simulator
+    ''' + cmd + '''
+    exit
+    ''')
+    print stdout.read()
+    stdout.close()
+    stdin.close()
 
 def main():
+    exec_rabbit(sshBR)
+    exec_iotcloud(sshI)
+    exec_sensor(sshI)
     exec_storm(sshNZ, 20, 4)
+    run_test(sshIR, 'sim', 4, 20, 'data/simbard_1.txt', 'true')
 
 if __name__ == "__main__":
     main()
