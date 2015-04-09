c8ac15d7a9fea12ea78e6c59b92a6fcc16530e04
Supun <supun06@gmail.com>
2015-04-09 00:00:29 -0400

    removing intermediate object creation

--- simulator/testscript/test.py
+++ simulator/testscript/test.py
@@ -3,15 +3,15 @@ import time
 
 sshNZ = paramiko.SSHClient()
 sshNZ.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-sshNZ.connect('10.39.1.18', username='ubuntu', key_filename='skamburu-key')
+sshNZ.connect('10.39.1.18', username='ubuntu', key_filename='/home/ubuntu/skamburu-key')
 
 sshBR = paramiko.SSHClient()
 sshBR.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-sshBR.connect('10.39.1.28', username='ubuntu', key_filename='skamburu-key')
+sshBR.connect('10.39.1.28', username='ubuntu', key_filename='/home/ubuntu/skamburu-key')
 
 sshI = paramiko.SSHClient()
 sshI.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-sshI.connect('10.39.1.26', username='ubuntu', key_filename='skamburu-key')
+sshI.connect('10.39.1.26', username='ubuntu', key_filename='/home/ubuntu/skamburu-key')
 
 def exec_storm(ssh, particles, parallel):
     stdin, stdout, stderr = ssh.exec_command('cd deploy/stom; ./bin/storm kill slam_processor -w 1')
