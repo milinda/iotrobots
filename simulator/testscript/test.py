4374a279c711c12126245015c1e296a8dc437536
Supun <supun06@gmail.com>
2015-04-17 19:28:10 -0400

    removing intermediate object creation

--- simulator/testscript/test.py
+++ simulator/testscript/test.py
@@ -131,7 +131,7 @@ def exec_sensor(ssh):
     stdout = channel.makefile('rb')
     stdin.write('''
     cd deploy/iotcloud2-bin-1.0-SNAPSHOT
-    ./bin/iotcloud jar repository/sensors/iotrobots-slam-sensor-1.0-SNAPSHOT-jar-with-dependencies.jar cgl.iotrobots.slam.sensor.SlamSensor -s local -sim -url "amqp://10.39.1.28:5672"  > /dev/null 2>&1 &
+    ./bin/iotcloud jar repository/sensors/iotrobots-slam-sensor-1.0-SNAPSHOT-jar-with-dependencies.jar cgl.iotrobots.slam.sensor.SlamSensor -s local -sim -url "amqp://10.23.0.93:5672"  > /dev/null 2>&1 &
     exit
     ''')
     print stdout.read()
@@ -141,7 +141,7 @@ def exec_sensor(ssh):
 
 def run_test(ssh, test, parallel, particles, input, simbad):
     print "running test...."
-    cmd = 'java -cp target/simulator-1.0-SNAPSHOT-jar-with-dependencies.jar cgl.iotrobots.sim.FileBasedDistributedSimulator "amqp://10.39.1.28:5672" ' + str(input) + ' results_dir7/' +str(test) + '/' + str(particles) + '_' + str(parallel) + ' ' +str(simbad) + ' false 1000'
+    cmd = 'java -cp target/simulator-1.0-SNAPSHOT-jar-with-dependencies.jar cgl.iotrobots.sim.FileBasedDistributedSimulator "amqp://10.23.0.93:5672" ' + str(input) + ' results_dir7/' +str(test) + '/' + str(particles) + '_' + str(parallel) + ' ' +str(simbad) + ' false 1000'
     channel = ssh.invoke_shell()
     stdin = channel.makefile('wb')
     stdout = channel.makefile('rb')
