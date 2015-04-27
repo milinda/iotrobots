cfa1448862191a8aebe1821905d0d175c3293200
Supun <supun06@gmail.com>
2015-04-27 00:37:57 -0400

    changing deploy scripts

--- simulator/testscript/Aces.java
+++ simulator/testscript/Aces.java
@@ -91,7 +91,7 @@ public class GFSAlgorithm {
         llsamplestep = 0.01;
         lasamplerange = 0.005;
         lasamplestep = 0.005;
-        maxCorrections = 10000;
+        maxCorrections = 140;
     }
 
     public void setParticles(int particles) {
