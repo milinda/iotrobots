faacfb1d5ca5f4b7524684e7f03f26eae1a850f3
Ubuntu <ubuntu@iot-01.(none)>
2015-04-27 00:13:57 +0000

    adding more

--- simulator/testscript/test.py
+++ simulator/testscript/test.py
@@ -144,7 +144,7 @@ simbard_times = {20: [300, 300, 300, 300, 300], 60: {400, 400, 400, 400, 400}, 1
 
 def run_test(ssh, test, parallel, particles, input, simbad):
     print "running test...."
-    cmd = 'java -cp target/simulator-1.0-SNAPSHOT-jar-with-dependencies.jar cgl.iotrobots.sim.FileBasedDistributedSimulator "amqp://10.39.1.28:5672" ' + str(input) + ' results_dir21/' +str(test) + '/' + str(particles) + '_' + str(parallel) + ' ' +str(simbad) + ' false 1000'
+    cmd = 'java -cp target/simulator-1.0-SNAPSHOT-jar-with-dependencies.jar cgl.iotrobots.sim.FileBasedDistributedSimulator "amqp://10.39.1.28:5672" ' + str(input) + ' results_dir22/' +str(test) + '/' + str(particles) + '_' + str(parallel) + ' ' +str(simbad) + ' false 1000'
     channel = ssh.invoke_shell()
     stdin = channel.makefile('wb')
     stdout = channel.makefile('rb')
