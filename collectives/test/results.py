469bc254be7b11a6a788853d24105056f62f14f4
supunkamburugamuva <supun06@gmail.com>
2015-05-13 13:51:37 -0400

    adding more to broadcast topology

---
+++ collectives/test/results.py
@@ -0,0 +1,26 @@
+
+def average(file_name, upperbound):
+    with open(file_name, "r") as in_f:
+        numbers = []
+        for line in in_f:
+            line = line.strip() # remove whitespace
+            if line: # make sure there is something there
+                number_on_line = int(line)
+                if number_on_line < upperbound:
+                    numbers.append(number_on_line)
+        sum_of_numbers = sum(numbers)
+        avg_of_numbers = sum(numbers)/len(numbers)
+
+    return avg_of_numbers
+
+def main():
+    tasks = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+    data = [100, 1000, 10000, 100000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000]
+    for d in data:
+        s = str(d) + ": "
+        for t in tasks:
+            s = s + " " + str(average("../bcast/" + str(d)  + "_" + str(t), 60))
+        print s
+
+if __name__ == "__main__":
+    main()
